e9b1b391789cb0b12d694e14704c374b47f803e0
dx-mon <dx-mon@users.sourceforge.net>
2021-06-21 15:09:40 +0100

    Corrected the platform header guard macro

--- include/usb/platform.hxx
+++ include/usb/platform.hxx
@@ -1,9 +1,9 @@
-#ifndef PLATFORM___HXX
-#define PLATFORM___HXX
+#ifndef USB_PLATFORM___HXX
+#define USB_PLATFORM___HXX
 
 #if defined(TM4C123GH6PM)
 #include <tm4c123gh6pm/platform.hxx>
 #include <tm4c123gh6pm/constants.hxx>
 #endif
 
-#endif /*PLATFORM___HXX*/
+#endif /*USB_PLATFORM___HXX*/
