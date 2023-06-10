728449a8d62929052b1c3651dc53cfeee3c3cb44
dragonmux <git@dragonmux.network>
2023-06-10 11:39:09 +0100

    stm32f1/core: Implemented stack initialisation

--- src/stm32f1/core.cxx
+++ src/stm32f1/core.cxx
@@ -18,6 +18,34 @@ using namespace usb::core::internal;
 
 namespace usb::core
 {
+	void init() noexcept
+	{
+		// Enable the clocks for the USB peripheral
+		rcc.apb1PeriphClockEn |= vals::rcc::apb1PeriphClockEnUSB;
+		// Enable the clocks for the GPIO Port A peripheral
+		rcc.apb2PeriphClockEn |= vals::rcc::apb2PeriphClockEnGPIOPortA;
+
+		// Set up the port pins used by the USB controller so they're in the right modes
+		// with the USB controller connected through.
+		vals::gpio::config<vals::gpio_t::pin8>(gpioA, vals::gpio::mode_t::input, vals::gpio::config_t::inputFloating);
+
+		// Having configured the pins, we now need to set up the USB controller
+		// Clear the control register, releasing power down and forced reset on the controller,
+		// set the buffer table pointer to the start of the USB SRAM, and clear pending interrupts.
+		usbCtrl.ctrl &= vals::usb::controlMask;
+		usbCtrl.bufferTablePtr = 0;
+		usbCtrl.intStatus &= vals::usb::itrStatusClearMask;
+
+		// Enable interrupts used by the implementation
+		usbCtrl.ctrl |= vals::usb::controlResetItrEn | vals::usb::controlCorrectXferItrEn |
+			vals::usb::controlSuspendItrEn | vals::usb::controlWakeupItrEn;
+
+		// Initialise the state machine
+		usbState = deviceState_t::detached;
+		usbCtrlState = ctrlState_t::idle;
+		usbDeferalFlags = 0;
+	}
+
 	void address(const uint8_t value) noexcept
 	{
 		usbCtrl.address = (usbCtrl.address & vals::usb::addressClrMask) |
