aed12669eaa9e511c34733c6b86beac27e6d8af7
dragonmux <git@dragonmux.network>
2023-06-12 00:30:02 +0100

    stm32f1/device: Implemented handling for control packets

--- src/stm32f1/device.cxx
+++ src/stm32f1/device.cxx
@@ -1,1 +1,28 @@
 // SPDX-License-Identifier: BSD-3-Clause
+#include "usb/platform.hxx"
+#include "usb/internal/core.hxx"
+#include "usb/internal/device.hxx"
+
+using namespace usb::constants;
+using namespace usb::types;
+using namespace usb::core;
+using namespace usb::core::internal;
+using namespace usb::descriptors;
+using namespace usb::device::internal;
+
+namespace usb::device
+{
+	void handleControlPacket() noexcept
+	{
+		// If we received a packet..
+		if (usbPacket.dir() == endpointDir_t::controllerOut)
+		{
+			if (usbCtrl.epCtrlStat[0] & vals::usb::epStatusSetup)
+				handleSetupPacket();
+			else
+				handleControllerOutPacket();
+		}
+		else
+			handleControllerInPacket();
+	}
+} // namespace usb::device
