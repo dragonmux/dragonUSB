d242eb072bdee13e3426616e5ec8b7faba4c7f97
dragonmux <git@dragonmux.network>
2024-06-21 00:17:20 -0700

    dwc2/otg: Defined the first tranch of configuration register constants

--- include/usb/dwc2/otg.hxx
+++ include/usb/dwc2/otg.hxx
@@ -107,6 +107,77 @@ namespace usb::dwc2
 		volatile uint32_t powerClockGateCtrl;
 	};
 	// NOLINTEND(cppcoreguidelines-avoid-const-or-ref-data-members)
+
+	enum class ahbBurstLength_t : uint8_t
+	{
+		single32b = 0U,
+		incr32b = 1U,
+		incr4x32b = 3U,
+		incr8x32b = 5U,
+		incr16x32b = 7U,
+	};
+
+	// Global OTG control/status register constants
+	constexpr static uint32_t globalOGTCtrlStatusSessionReqStatusMask{1U << 0U};
+	constexpr static uint32_t globalOGTCtrlStatusSessionReqStatusFailure{0U << 0U};
+	constexpr static uint32_t globalOGTCtrlStatusSessionReqStatusSuccess{1U << 0U};
+	constexpr static uint32_t globalOGTCtrlStatusSessionReq{1U << 1U};
+	constexpr static uint32_t globalOGTCtrlStatusVBusValidOverrideEnable{1U << 2U};
+	constexpr static uint32_t globalOGTCtrlStatusVBusValidOverrideValue{1U << 3U};
+	constexpr static uint32_t globalOGTCtrlStatusASessValidOverrideEnable{1U << 4U};
+	constexpr static uint32_t globalOGTCtrlStatusASessValidOverrideValue{1U << 5U};
+	constexpr static uint32_t globalOGTCtrlStatusBSessValidOverrideEnable{1U << 6U};
+	constexpr static uint32_t globalOGTCtrlStatusBSessValidOverrideValue{1U << 7U};
+	constexpr static uint32_t globalOGTCtrlStatusHostNegStatusMask{1U << 8U};
+	constexpr static uint32_t globalOGTCtrlStatusHostNegStatusFailure{0U << 8U};
+	constexpr static uint32_t globalOGTCtrlStatusHostNegStatusSuccess{1U << 8U};
+	constexpr static uint32_t globalOGTCtrlStatusHNPRequest{1U << 9U};
+	constexpr static uint32_t globalOGTCtrlStatusHostSetHNPEnable{1U << 10U};
+	constexpr static uint32_t globalOGTCtrlStatusDeviceHNPEnale{1U << 11U};
+	constexpr static uint32_t globalOGTCtrlStatusEmbeddedHostEnable{1U << 12U};
+	constexpr static uint32_t globalOGTCtrlStatusConnIDMask{1U << 16U};
+	constexpr static uint32_t globalOGTCtrlStatusConnIDA{0U << 16U};
+	constexpr static uint32_t globalOGTCtrlStatusConnIDB{1U << 16U};
+	constexpr static uint32_t globalOGTCtrlStatusDebounceMask{1U << 17U};
+	constexpr static uint32_t globalOGTCtrlStatusDebounceLong{0U << 17U};
+	constexpr static uint32_t globalOGTCtrlStatusDebounceShort{1U << 17U};
+	constexpr static uint32_t globalOGTCtrlStatusASessValid{1U << 18U};
+	constexpr static uint32_t globalOGTCtrlStatusBSessValid{1U << 19U};
+	constexpr static uint32_t globalOGTCtrlStatusOTGMask{1U << 20U};
+	constexpr static uint32_t globalOGTCtrlStatusOTG1_3{0U << 20U};
+	constexpr static uint32_t globalOGTCtrlStatusOTG2_0{1U << 20U};
+	constexpr static uint32_t globalOGTCtrlStatusCurrentModeMask{1U << 21U};
+	constexpr static uint32_t globalOGTCtrlStatusCurrentModeDevice{0U << 21U};
+	constexpr static uint32_t globalOGTCtrlStatusCurrentModeHost{1U << 2U};
+
+	// Global OTG interrupt status register constants
+	constexpr static uint32_t globalOTGInterruptSessionEndDetected{1U << 2U};
+	constexpr static uint32_t globalOTGInterruptSessionReqStatusChange{1U << 8U};
+	constexpr static uint32_t globalOTGInterruptHostNegStatusChange{1U << 9U};
+	constexpr static uint32_t globalOTGInterruptHostNegDetected{1U << 17U};
+	constexpr static uint32_t globalOTGInterruptATimeoutChange{1U << 18U};
+	constexpr static uint32_t globalOTGInterruptDebounceDone{1U << 19U};
+
+	// Global AHB configuration register constants
+	constexpr static uint32_t globalAHBConfigGlobalIntUnmask{1U << 0U};
+	constexpr static uint32_t globalAHBConfigBurstLengthMask{0xfU << 1U};
+	constexpr static uint32_t globalAHBConfigDMAEnable{1U << 5U};
+	constexpr static uint32_t globalAHBConfigTxFIFONotEmpty{1U << 7U};
+	constexpr static uint32_t globalAHBConfigPeriodicFIFOEmpty{1U << 8U};
+
+	constexpr inline uint32_t globalAHBConfigBurstLength(const ahbBurstLength_t burstLength) noexcept
+		{ return static_cast<uint32_t>(static_cast<uint8_t>(burstLength) & 0xfU) << 1U; }
+
+	// Global reset register constants
+	constexpr static uint32_t globalResetCtrlCore{1U << 0U};
+	constexpr static uint32_t globalResetCtrlPartial{1U << 1U};
+	constexpr static uint32_t globalResetCtrlHostFrameCounter{1U << 2U};
+	constexpr static uint32_t globalResetCtrlRxFIFOFlush{1U << 4U};
+	constexpr static uint32_t globalResetCtrlTxFIFOFlush{1U << 5U};
+	constexpr static uint32_t globalResetCtrlTxFIFONumberMask{0x1fU << 6U};
+	constexpr static size_t globalResetCtrlTxFIFONumberShift{6U};
+	constexpr static uint32_t globalResetCtrlDMAReqOngoing{1U << 30U};
+	constexpr static uint32_t globalResetCtrlAHBIdle{1U << 31U};
 } // namespace usb::dwc2
 
 #endif /*USB_DWC2_OTG_HXX*/
